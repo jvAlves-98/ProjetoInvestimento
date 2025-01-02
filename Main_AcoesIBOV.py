68704108b3adb55a38e8c59cae8acfce37cb4361
jvAlves-98 <jv.falves98@outlook.com>
2025-01-02 20:05:29 -0300

    2° Comit atualização ACOES_IBOV

--- Main_AcoesIBOV.py
+++ Main_AcoesIBOV.py
@@ -1,13 +1,21 @@
+import os
 import pandas as pd
 import yfinance as yf
+from datetime import datetime, timedelta
+
+# Variaveis para execução de codigos e caminhos para arquivos
+db_AcoesIBOV = r"C:\Users\João Vitor\OneDrive\Workspace - Python VS Code\Projetos - Produção\ProjetoInvestimento\Historico cotações\Ações IBOV"
+
+# Data inicial para  busca dos dados
+data_inicial = '2021-01-01'
 
 
 def obter_tickers() -> list:
     """
-    Processa o arquivo CSV com a lista de tickerts
+    Processa o arquivo CSV com a lista de tickerts.
     CSV retirado do Status Invest link: https://statusinvest.com.br/acoes/busca-avancada
-    Retirada todas as colunas não relevantes
-    Adicionado '.SA' pois o Yahoo Finance reconhece somente dessa forma
+    Retirada todas as colunas não relevantes.
+    Adicionado '.SA' pois o Yahoo Finance reconhece somente dessa forma.
     """
 
     caminho_csv = 'statusinvest-busca-avancada.csv'
@@ -21,53 +29,82 @@ def obter_tickers() -> list:
     return ListaAcoes['TICKER'].tolist()
 
 
-def obter_historico(StartDate, EndDate):
-    dfHistorico = []
+def obter_historico(StartDate, EndDate, Tickers):
+    """
+    Obtem o historico de cada ticker na data especificada.
+    É feita cada consulta separadamente reduzindo o tempo de cada consulta.
+    """
 
-    for Tickers in obter_tickers():
-        try:
-            # Download das cotações do yfinance
-            dfTicker = yf.download(Tickers, start=StartDate, end=EndDate)
+    try:
+        # Download das cotações do yfinance
+        dfTicker = yf.download(Tickers, start=StartDate, end=EndDate,
+                               actions=True, ignore_tz=True, rounding=True, multi_level_index=False, progress=False)
 
-            if dfTicker.empty:
-                print(f"Nenhum dado encontrado para o ticker: {Tickers}")
-                continue
-            # Resentando o index do data frame para retirar a coluna "Date" do index
-            dfTicker = dfTicker.reset_index()
-            dfTicker.columns = dfTicker.columns.droplevel(1)
-            dfTicker.columns = ['Date', 'Close',
-                                'High', 'Low', 'Open', 'Volume']
+        if dfTicker.empty:
+            # print(f"Nenhum dado encontrado para o ticker: {Tickers}")
+            return None
 
-            """
-            Corrijinfo e normalizando a tabela.
-            Arredondando os numeros e convertendo para o padrão brasileiro: "," separador de casas decimais.
-            
-            """
-            colunas_numericas = ['Close', 'High', 'Low', 'Open']
-            dfTicker[colunas_numericas] = dfTicker[colunas_numericas].round(2)
-            dfTicker[colunas_numericas] = dfTicker[colunas_numericas].applymap(
-                lambda x: f"{x: .2f}".replace('.', ',')
-            )
+        # Resentando o index do data frame para retirar a coluna "Date" do index
+        dfTicker = dfTicker.reset_index()
+        dfTicker = dfTicker.drop(columns=['Stock Splits'])
+        dfTicker.columns = ['Date', 'Close',
+                            'Dividends', 'High', 'Low', 'Open', 'Volume']
 
-            # Adicionando a coluna dos Tickers e todos os tickers em um unico DataFrame
-            dfTicker['Ticker'] = Tickers
-            dfHistorico.append(dfTicker)
-        except Exception as e:
-            print(f"Erro ao processar o ticker {Tickers}: {e}")
-            continue
+        # Normalizando colunas de valores
+        colunas_numericas = ['Close', 'Dividends', 'High', 'Low', 'Open']
+        dfTicker[colunas_numericas] = dfTicker[colunas_numericas].map(
+            lambda x: f"{x: .2f}".replace('.', ',')
+        )
 
-    dfFinal = pd.concat(dfHistorico, ignore_index=True)
-    return dfFinal
+        # Adicionando a coluna dos Tickers e todos os tickers em um unico DataFrame
+        dfTicker['Ticker'] = Tickers
+
+        return dfTicker
+
+    except Exception as e:
+        # print(f"Erro ao processar o ticker {Tickers}: {e}")
+        return None
 
 
-StartDate = '2024-01-02'
-EndDate = '2024-12-30'
+def salvar_historico(data_inicial, db_AcoesIBOV):
+    """
+    Gera o histórico mensalmente a partir de uma data inicial e salva todos os tickers em um arquivo mensal.
+    Salva no diretorio especifico criando um "DataBase"
+    """
 
-dfFinal = obter_historico(StartDate, EndDate)
+    # Data atual é a final a ser usada (Pode ser usado o dia anterior depende do horario de execução)
+    data_final = datetime.now()
+    data_atual = datetime.strptime(data_inicial, "%Y-%m-%d")
 
-# Salvar o DataFrame final em um arquivo CSV
-dfFinal.to_csv('Historio\Historico Ações\dados_historico.csv',
-               sep=';', index=False)
+    # Iterar mensalmente
+    while data_atual < data_final:
+        # Definir o início e o fim do mês atual
+        proximo_mes = data_atual + timedelta(days=31)
+        fim_mes = (proximo_mes.replace(day=1) - timedelta(days=1))
 
-# Exibir parte do DataFrame final
-print(dfFinal.head())
+        dfMes = []  # DataFrame para consolidar os dados do mês
+
+        # Processa cada ticker e obter o historico do mes
+        for ticker in obter_tickers():
+            # print(f'Processando {ticker} de {data_atual} a {fim_mes}')
+            dfHistorico = obter_historico(data_atual, fim_mes, ticker)
+            if dfHistorico is not None:
+                dfMes.append(dfHistorico)
+
+        # Concatena os dados de todos o ticker no mês
+        if dfMes:
+            dfMes = pd.concat(dfMes, ignore_index=True)
+
+            # Criando o nome do arquivo para o mes e ano
+            nome_arquivo = os.path.join(db_AcoesIBOV, f'Acoes_IBOV_{
+                                        data_atual.strftime("%m")}_{data_atual.strftime("%Y")}.csv')
+
+            # Salvar o arquivo
+            dfMes.to_csv(nome_arquivo, sep=';', index=False)
+            # print(f'Arquivo salvo em: {nome_arquivo}')
+
+        # Iterar a data atual para o mes seguinte
+        data_atual = proximo_mes.replace(day=1)
+
+
+salvar_historico(data_inicial, db_AcoesIBOV)
